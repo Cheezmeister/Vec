1957d2b4f8dddd14b35622f0d04fe64fba4c04ed
Brendan Luchen <WindowsBox>
2015-04-01 02:12:55 -0700

    crossgl.h

---
+++ src/crossgl.h
@@ -0,0 +1,13 @@
+/* This is why we can't have nice things. */
+#if _WIN32
+#include <Windows.h>
+#include <GL/glew.h>
+#include <GL/gl.h>
+#elif __APPLE__ 
+#include <GL/glew.h>
+#include <OpenGL/gl.h>
+#else
+#include <GL/glew.h>
+#include <GL/gl.h>
+#endif
+
